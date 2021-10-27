894e48141001cb5ce21ec356c8483955c0b93600
itsmefox <patrick.boesch@viascom.ch>
2021-10-27 14:10:43 +0200

    feat: add logger

--- src/main/java/io/viascom/discord/bot/Application.java
+++ src/main/java/io/viascom/discord/bot/Application.java
@@ -3,8 +3,12 @@ package io.viascom.discord.bot;
 import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
+
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws InterruptedException, LoginException {
         var properties = new ApplicationProperties();
@@ -23,9 +27,9 @@ public class Application {
         //You can get the PERMISSIONS INTEGER on the discord developer page
         var inviteLink = "https://discordapp.com/oauth2/authorize?client_id=" + clientId + "&scope=bot%20applications.commands&permissions=137439333440";
 
-        System.out.println("==================");
-        System.out.println("Invite me: " + inviteLink);
-        System.out.println("==================");
+        logger.info("==================");
+        logger.info("Invite me: " + inviteLink);
+        logger.info("==================");
     }
 
 }
