820cf6ea26fe146a21a017a7b2e53540d4147083
alex-skxy <alex13skx@gmail.com>
2021-10-30 01:43:58 +0200

    make the test an actual test ;)

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,14 +1,13 @@
-import ch.yth2021.charjar.API.model.APIRespondedBullshitException;
 import ch.yth2021.charjar.discord.module.swear.SwearModule;
 import org.junit.Test;
 
-import java.io.IOException;
+import static org.junit.Assert.assertEquals;
 
 public class UserServiceTest {
     @Test
-    public void test01() throws APIRespondedBullshitException, IOException {
+    public void test01() {
         SwearModule sm = new SwearModule();
         int i = sm.countSwearwords("how many fu*king wear words does this shit have ? i dont fucking know");
-        System.out.println(i);
+        assertEquals(3, i);
     }
 }
