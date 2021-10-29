81243f226438f6e9f372ff2faf292f659d23cf62
alex-skxy <alex13skx@gmail.com>
2021-10-30 01:47:07 +0200

    create more tests

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -10,4 +10,25 @@ public class UserServiceTest {
         int i = sm.countSwearwords("how many fu*king wear words does this shit have ? i dont fucking know");
         assertEquals(3, i);
     }
+
+    @Test
+    public void test02() {
+        SwearModule sm = new SwearModule();
+        int i = sm.countSwearwords("fuckass");
+        assertEquals(1, i);
+    }
+
+    @Test
+    public void test03() {
+        SwearModule sm = new SwearModule();
+        int i = sm.countSwearwords("this fucking fuck");
+        assertEquals(2, i);
+    }
+
+    @Test
+    public void test04() {
+        SwearModule sm = new SwearModule();
+        int i = sm.countSwearwords("hi everyone!");
+        assertEquals(0, i);
+    }
 }
